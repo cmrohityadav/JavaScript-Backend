606ba6d14f15d03ab14de6baf52b3854badc4bbf
cmrohityadav <cmrohityadav23@gmail.com>
2023-09-29 20:23:58 +0530

    basic setup

---
+++ 01_/index.js
@@ -0,0 +1,23 @@
+// console.log("hello Rohit ")
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/twitter',(req,res)=>{
+    res.send('cmrohityadav')
+})
+
+app.get('/login',(bhikh,daan)=>{
+    daan.send('<h1 style=color:red> Daan kro yaaro login kar ke<h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
--- 01_/notesForMe.md
+++ 01_/notesForMe.md
@@ -1,3 +1,14 @@
+
+steps
+
 1. npm init
 
-2. 
+2. "start": "node index.js"
+
+3. npm install express
+
+4. npm i dotenv
+
+5. add .env file
+
+6. require('dotenv').config()
