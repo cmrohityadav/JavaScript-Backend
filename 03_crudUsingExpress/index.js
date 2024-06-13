c01be45ae45aa0c13ab9d6d3cfe79ae9f2777f0f
cmrohityadav <cmrohityadav23@gmail.com>
2024-06-13 18:10:47 +0530

    morgon and wisdom

--- 03_crudUsingExpress/index.js
+++ 03_crudUsingExpress/index.js
@@ -1,6 +1,9 @@
 import 'dotenv/config'
 import express from "express"
+import logger from './logger.js';
+import morgan from 'morgan';
 
+const morganFormat = ':method :url :status :response-time ms';
 const app=express()
 const port= process.env.PORT || 3000
 
@@ -8,6 +11,20 @@ app.use(express.json())
 
     let itemData=[]
     let nextId=1
+app.use(morgan(morganFormat, {
+        stream: {
+          write: (message) => {
+            const logObject = {
+              method: message.split(' ')[0],
+              url: message.split(' ')[1],
+              status: message.split(' ')[2],
+              responseTime: message.split(' ')[3],
+      
+            };
+            logger.info(JSON.stringify(logObject));
+          }
+        }
+      }));    
 
     // add
 app.post("/add",(req,res)=>{
