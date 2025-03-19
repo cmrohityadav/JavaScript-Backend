507f417962a3b2cdbd326dd24e09830ba6c9454a
cmrohityadav <cmrohityadav23@gmail.com>
2025-03-20 00:46:53 +0530

    added error handler

--- 05_course/3_express/index.js
+++ 05_course/3_express/index.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const app = express();
 
-//Middlewares
+//Middlewares 1 way
 app.use(function(req,res,next){
     console.log("Middleware doing critical operations")
     
@@ -18,7 +18,21 @@ app.use(function(req,res,next){
 
 app.use(function(req,res,next){
     console.log("middleware 2 working ek baar auur")
+    next()
 })
+
+
+//Middlewares 2 way
+function middlewareCheckAdmin(req,res,next){
+    console.log("Admin varifying using db call...")
+    next()
+}
+app.get("/admin",middlewareCheckAdmin,(req,res)=>{
+    res.send("U are  verified ADMIN")
+})
+
+
+
 
 //routes apiendpoint all are same just change the function type
 //works same
@@ -38,7 +52,14 @@ function requestHandler(req, res) {
 app.get("/profile", requestHandler);
 
 
-
+//always use in last for error middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+        message: err.message || "Internal Server Error",
+        status: err.status || 500,
+    });
+});
 
 
 app.listen(3000)
