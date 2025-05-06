dcb62931cf2647f502f20b7fb19e26b860439d7a
cmrohityadav <cmrohityadav23@gmail.com>
2025-05-06 23:51:58 +0530

    added register api

--- Projects/02_miniProject/server.js
+++ Projects/02_miniProject/server.js
@@ -1,4 +1,6 @@
 import express from "express"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 const app=express()
 
 import userTable from "./models/user.js"
@@ -15,11 +17,32 @@ import cookieParser from "cookie-parser"
 app.use(cookieParser());
 
 
-app.post("/register",(req,res)=>{
+app.post("/register",async(req,res)=>{
     const {username,name,age,email,password}=req.body
+    let user=await userTable.findOne({email})
     
+    if(!user) return res.status(401).send("User already registered")
+    
+    bcrypt.genSalt(10,(error,salt)=>{
+        bcrypt.hash(password,salt,async(error,hash)=>{
+        let  user= await userTable.create({
+                username,
+                name,
+                email,
+                age,
+                password:hash
+            });
+           let token= jwt.sign({email:email,userId:user._id},"secretkey")
+           res.cookie("token",token);
+           res.status(200).send("registered")
+        })
+
+    })
+
 })
 const PORT=3000;
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 });
+
+
