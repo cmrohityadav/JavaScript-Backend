3650b3ce7efe6b3b8d13f34ce90dd70653b9b691
cmrohityadav <cmrohityadav23@gmail.com>
2025-07-09 22:38:59 +0530

    added get book and add bokk controller

--- Projects/03_BookStore/controllers/BookController.js
+++ Projects/03_BookStore/controllers/BookController.js
@@ -1,5 +1,29 @@
+import BookCollection from "../models/BookModel.js";
 
 const getAllBooks=async(req,res)=>{
+    try {
+        const allBook= await BookCollection.find();
+        if(!allBook){
+            res.status(200).json({
+            success:false,
+            message:'No book avaliable',
+            
+        }) 
+        return;
+        }
+        res.status(200).json({
+            success:true,
+            message:'All books',
+            data:allBook,
+        }) ;
+    } catch (error) {
+    console.error("Error in getAllBooks():  ",error)
+    res.status(400).json({
+            success:false,
+            message:'got get all book',
+            
+        }) 
+    }
 
 }
 
@@ -8,7 +32,27 @@ const getSingleBookById=async(req,res)=>{
 }
 
 const addNewBook=async(req,res)=>{
+try {
+    const bodyBookData=req.body;
+    const responseDocAfterAddingBook=await  new BookCollection(bodyBookData).save();
+    if(responseDocAfterAddingBook){
+        res.status(200).json({
+            success:true,
+            message:'Book added successfully',
+            data:responseDocAfterAddingBook,
+        })
+    }
+    return;
+} catch (error) {
 
+    console.error("Error in addNewBook():  ",error)
+    res.status(400).json({
+            success:false,
+            message:'Book add unsuccessfully',
+            
+        })
+    
+}
 }
 
 const updateBook=async(req,res)=>{
